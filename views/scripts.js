0eabc46c083a5ce07d16b04a3aa0c59751d0860c
christopher coyne <christcoyne@gmail.com>
2022-07-30 21:12:56 -0400

    script for playing videos on hover

--- views/scripts.js
+++ views/scripts.js
@@ -1,8 +1,37 @@
 console.log("hello world");
 
+const clips = document.querySelectorAll(".clip");
+for (let i = 0; i < clips.length; i++) {
+  clips[i].addEventListener("mouseenter", function (e) {
+    clips[i].play();
+  });
+  clips[i].addEventListener("mouseout", function (e) {
+    clips[i].pause();
+  });
+}
+
+/*
+var video = document.querySelector(".video");
+var videoFiller = document.querySelector(".videoFiller");
+var gif = document.createElement("img");
+gif.src = "./videos/largescreentest.gif";
+
+video.addEventListener("mouseover", (event) => {
+  console.log("hovered!!!");
+  video.replaceChild(gif, videoFiller);
+});
+
+gif.addEventListener("mouseout", (event) => {
+  console.log("un hovered");
+  video.replaceChild(videoFiller, gif);
+});
+*/
+
+/*
 const myfreeze = new Freezeframe(".freezeframe", {
   trigger: "hover",
   overlay: false,
 });
 
 console.log("freeze frame : ", myfreeze);
+*/
